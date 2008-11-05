d8d4887dd2934c6bcab9728413c9c11d791b3a20
Larry Diehl <larrytheliquid@larry-diehls-macbook.local>
2008-11-05 11:36:24 -0500

    Added Population and .initialized

--- revolve.rb
+++ revolve.rb
@@ -2,3 +2,4 @@ module Revolve end
   
 require File.join(File.dirname(__FILE__), "lib", "method")
 require File.join(File.dirname(__FILE__), "lib", "program")
+require File.join(File.dirname(__FILE__), "lib", "population")
