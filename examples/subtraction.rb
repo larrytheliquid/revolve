803b5fe07e5fea39f30b3fdcd9c10983ca1ee593
Larry Diehl <larrytheliquid@host88.orl2.omnihsiaspnt.com>
2008-11-06 12:54:47 -0500

    Got subtraction to converge

--- examples/subtraction.rb
+++ examples/subtraction.rb
@@ -6,6 +6,16 @@ class Integer
   end
 end
 
+def cases(num, step, first, second)
+  (1..num).map do |i|
+    lambda do |program|
+      first = first + step*i
+      program.run( Revolve::Argument.new(:x, first), 
+                   Revolve::Argument.new(:y, second)).to_i - (first - second)
+    end
+  end
+end
+
 population = Revolve::Population.initialized( 200, {  
   :program_size => 10,
   :instructions => [ 1, 2, 3, 4, 5, 6, 7, 8, 9,
@@ -13,14 +23,7 @@ population = Revolve::Population.initialized( 200, {
                      Revolve::Method.new(:*), Revolve::Method.new(:protected_division),
                      Revolve::Variable.new(:x), Revolve::Variable.new(:y) ],
   :max_generations => 500,                    
-  :fitness_cases => [ lambda{|program| program.run( Revolve::Argument.new(:x, 13),
-                                                    Revolve::Argument.new(:y, 10)).to_i - 3 },
-                      lambda{|program| program.run( Revolve::Argument.new(:x, 25),
-                                                    Revolve::Argument.new(:y, 64)).to_i - -39 },
-                      lambda{|program| program.run( Revolve::Argument.new(:x, 900),
-                                                    Revolve::Argument.new(:y, 367)).to_i - 533 },
-                      lambda{|program| program.run( Revolve::Argument.new(:x, 56),
-                                                    Revolve::Argument.new(:y, -800)).to_i - -734 }],
+  :fitness_cases => cases(20, 6, 10, 34),
   :fitness_combinator => lambda{|cases| cases.inject{|x, y| x.abs + y.abs } },
   :crossover_percent => 0.6,
   :mutation_percent => 0.3
--- revolve.rb
+++ revolve.rb
@@ -6,5 +6,7 @@ require File.join(File.dirname(__FILE__), "lib", "method")
 require File.join(File.dirname(__FILE__), "lib", "program")
 require File.join(File.dirname(__FILE__), "lib", "population")
 
+# TODO: Make fitness case a class instead of passing lambdas?
 # TODO: Elitism
 # TODO: Tournament selection only selects better fitness 75% of the time
+# TODO: Use Mocha in fitness cases
